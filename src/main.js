12f2de4d7e92ce3fd319019ddf37ffd9fd49c48e
Sebastian Zabrzyski <sebastianzabrzyski@gmail.com>
2023-01-23 09:52:56 +0100

    add bootstrap

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import * as bootstrap from 'bootstrap';
 import './assets/main.css'
+
+window.bootstrap = bootstrap;
 
 const app = createApp(App)
 
