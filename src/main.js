41879a2889cbc90b7c344c2a4bf8b4c5f8777f05
Sebastian Zabrzyski <sebastianzabrzyski@gmail.com>
2023-01-23 10:47:59 +0100

    add axios

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import * as bootstrap from 'bootstrap';
+import axios from 'axios';
 import './assets/main.css'
 
 window.bootstrap = bootstrap;
+window.axios = axios;
 
 const app = createApp(App)
 
